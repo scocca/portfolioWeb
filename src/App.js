afbcace80577a646da525796e2612976765aedbc
scocca <s.cocca.u@gmail.com>
2023-10-24 15:53:37 -0300

    se cambian imagenes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react'
 import jobsData from './workData.json'
 import skillsData from './skills.json'
 import './App.css'
-import hiim from './components/assets/hi im.svg'
-import aboutme from './components/assets/About Me.svg'
+
 import works from './components/assets/workingDecoration.svg'
 import photo1 from './components/assets/_DSC3911.JPG' 
 import photo2 from './components/assets/20220711_171031.jpg'
 import photo3 from './components/assets/20230128_202758.jpg'
-import photo4 from './components/assets/20230413_172322.jpg'
+import photo4 from './components/assets/IMG_20210924_185053.jpg'
 import photo5 from './components/assets/IMG_20211024_023329.jpg'
 
 
